4866d109b2a0c65e55b390283f23c8edbc3e472c
Abhishek Iyengar <tinshade98@gmail.com>
2026-09-15 14:02:27 +0530

    Removing all experimentational dirs

--- temp.py
+++
@@ -1,8 +0,0 @@
-import os
-
-repos = ("langchain-experiments","log-watcher","rag-experiments","vector-db-experiments",)
-
-
-for each in repos:
-    directory = os.path.join(os.getcwd(), each)
-    os.remove(directory)
